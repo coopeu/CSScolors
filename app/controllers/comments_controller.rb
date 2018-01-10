93e399b20c42b7e9424b300108e69dc24d4b810e
coopeu <consen@consen.org>
2018-01-10 11:30:24 +0000

    mockup1

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Post.order('updated_at DESC').all
+	end
+	def new
+		@post = Post.new
+	end
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to posts_path
+		else
+			#
+		end
+	end
+	def show
+		@post = Post.where(id: params[:id]).first
+	end
+	def edit
+		@post = Post.where(id: params[:id]).first
+	end
+	def update
+		@post = Post.where(id: params[:id]).first
+		if @post.update_attributes(post_params)
+			redirect_to posts_path
+		else
+			# render the edit form again
+		end	
+	end
+	def destroy
+		@post = Post.where(id: params[:id]).first
+		if @post.destroy
+			redirect_to posts_path
+		else
+			# render the destroy form again
+		end	
+	end	
+private
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end	
+
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,13 @@ class PagesController < ApplicationController
 	def home
 	end
 
+	def about
+	end
+
+	def products
+	end
+
+	def contact
+	end
+
 end
